a48bf273a9e11d637e0746a6145d6f77a174f243
yunguid <123339269+yunguid@users.noreply.github.com>
2025-10-15 22:50:01 -0400

    item-17

---
+++ chapter-2/defaultdict_over_setdefault.py
@@ -0,0 +1,11 @@
+"""
+Handling missing keys. There are a variety of ways to do this
+
+For example, say I want to keep track of cities that I've visited in countries around the world
+Here we have a dictionary that maps country names to a set instance containing corresponding city names
+"""
+
+visits = {
+    "Mexico": {"Tulum", "Puerto Vallarta"},
+    "Japan": {"Hakone"},
+}
